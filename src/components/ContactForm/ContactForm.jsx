4fda7f4b7202d2b5df89d4f40203300965cc01fb
Spajkowiecki <pcio10@wp.pl>
2024-02-23 10:29:03 +0100

    HotFix
    'PropTypes' is defined but never used  no-unused-vars

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import style from './ContactForm.module.css';
-import PropTypes from 'prop-types';
+
 class ContactForm extends Component {
   handleSubmit = event => {
     event.preventDefault();
